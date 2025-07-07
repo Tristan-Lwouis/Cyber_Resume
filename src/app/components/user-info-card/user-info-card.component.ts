4ccf9bc058e0582ae4a6886eae4485245b0587f4
Lwouis <nocte.grafika@gmail.com>
2025-07-07 13:02:27 +0200

    Ajout du component userInfoCard

--- src/app/components/user-info-card/user-info-card.component.ts
+++ src/app/components/user-info-card/user-info-card.component.ts
@@ -1,14 +1,31 @@
 
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // OBLIGATOIRE pour ngStyle
 
 @Component({
   selector: 'app-user-info-card',
-  standalone: true, // ?? A quoi ca sert ?
-  imports: [],
+  //standalone: true, // ?? A quoi ca sert ?
+  imports: [CommonModule],
   templateUrl: './user-info-card.component.html',
   styleUrl: './user-info-card.component.scss'
 })
+
 export class UserInfoCardComponent {
     @Input() title: string = '';
     @Input() titleClass: string = '';
+
+  firstTabZ = 2;
+  secondTabZ = 1;
+
+  showFirstTab() {
+    this.firstTabZ = 2; // 2 pour être au-dessus de l'autre
+    this.secondTabZ = 1; // 1 pour être en dessous de l'autre
+    console.log("Appuie sur le premier onglet")
+  }
+
+  showSecondTab() {
+    this.firstTabZ = 1; // 1 pour être en dessous de l'autre
+    this.secondTabZ = 2; // 2 pour être au-dessus de l'autre
+    console.log("Appuie sur le deuxieme onglet")
+  }
 }
