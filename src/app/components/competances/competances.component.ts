893a3c9ed6fba76e0d3dde5bd0f32fc0655c2f8b
Tristan-Lwouis <regniertristan@gmail.com>
2025-08-12 13:43:03 +0200

    Modifications du composant Compétences - Ajout de la rotation de flèche - Mise en place du drag & drop - Amélioration du style et de la mise en page

--- src/app/components/competances/competances.component.ts
+++ src/app/components/competances/competances.component.ts
@@ -9,5 +9,23 @@ import { DragDropModule } from '@angular/cdk/drag-drop'; //Drag and Drop
   styleUrl: './competances.component.scss'
 })
 export class CompetancesComponent {
+  // Propriété pour suivre l'angle de rotation actuel
+  arrowRotation: number = 0;
+  open: boolean = false;
 
+  // Méthode appelée quand on clique sur la flèche
+  rotateArrow() {
+    // Ajoute 90 degrés à la rotation actuelle
+    this.arrowRotation += 90;
+    
+    // Optionnel : garder la rotation entre 0 et 360 degrés
+    if (this.arrowRotation > 90) {
+      this.arrowRotation = 0;
+      this.open = false;
+      // console.log("close")
+    } else {
+      this.open = true;
+      // console.log("open")
+    }
+  }
 }
